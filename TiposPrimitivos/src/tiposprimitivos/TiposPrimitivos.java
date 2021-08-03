b484653822f2864955dddc8125fe583c0d726e0e
Agladejesus <agladejesus@gmail.com>
2021-08-03 06:47:48 -0400

    Update TiposPrimitivos.java

--- TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
+++ TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
@@ -20,6 +20,7 @@ public class TiposPrimitivos {
         float nota = 8.5f;
         System.out.println("A nota é: " + nota);
         System.out.printf("A nota de %s é %.2f \n", nome, nota);
+        System.out.format("A nota de %s é %.2f \n", nome, nota);
     }
     
 }
