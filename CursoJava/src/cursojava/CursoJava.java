20108aa634d258153bbb04aa03356732e6ae5e55
Agladejesus <agladejesus@gmail.com>
2021-08-05 20:42:43 -0400

    Update CursoJava.java

--- CursoJava/src/cursojava/CursoJava.java
+++ CursoJava/src/cursojava/CursoJava.java
@@ -3,10 +3,34 @@ package cursojava;
 public class CursoJava {
 
     public static void main(String[] args) {
+      /*
+        int num=0;
+        int num1=10;
+        int num2=20;
+        int num3=30;
+        int res = num + num1 + num2 + num3;
+        String nome="Alexandre de Jesus";
+        
+        num=100;
+        num=150;
+        num= num + 20;
+        
         System.out.print("CFBCursos\n");
         System.out.println("CFBCursos");
         System.out.printf("Canal:%s%nAno:%d\n", "CFB Cursos", 2021);
         System.out.printf("Canal:%s\nAno:%d\n", "CFB Cursos", 2021);
+        System.out.printf("Valor da variavel res:%d\nNome:%s%n%n",res, nome);
+        */
+      
+      int nota=50;
+      int media=60;
+      
+      if(nota >= media){
+          System.out.println("Aprovado");
+      } else{
+          System.out.println("Reprovado");
+      }
+      System.out.println("Fim do programa");
     }
     
 }
