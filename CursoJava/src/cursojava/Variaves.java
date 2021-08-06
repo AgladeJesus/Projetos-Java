a221f926fc53594e465986e6f77c1b8c1fc86d99
AgladeJesus <agladejesus@gmail.com>
2021-08-06 09:35:24 -0400

    Create Variaves.java

---
+++ CursoJava/src/cursojava/Variaves.java
@@ -0,0 +1,23 @@
+package cursojava;
+
+public class Variaves {
+    public static void main(String[] args) {
+        int num=0;
+        int num1=10;
+        int num2=20;
+        int num3=30;
+        int res = num + num1 + num2 + num3;
+        String nome="Alexandre de Jesus";
+        
+        num=100;
+        num=150;
+        num= num + 20;
+        
+        System.out.print("CFBCursos\n");
+        System.out.println("CFBCursos");
+        System.out.printf("Canal:%s%nAno:%d\n", "CFB Cursos", 2021);
+        System.out.printf("Canal:%s\nAno:%d\n", "CFB Cursos", 2021);
+        System.out.printf("Valor da variavel res:%d\nNome:%s%n%n",res, nome);
+    }
+    
+}
