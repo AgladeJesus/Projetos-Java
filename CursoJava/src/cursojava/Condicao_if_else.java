6f61f7517729b8cdbcade2b24dba687cde2e783d
AgladeJesus <agladejesus@gmail.com>
2021-08-06 09:35:30 -0400

    Create Condicao_if_else.java

---
+++ CursoJava/src/cursojava/Condicao_if_else.java
@@ -0,0 +1,22 @@
+package cursojava;
+
+public class Condicao_if_else {
+    public static void main(String[] args) {
+      int nota=100;
+      int faltas=4;
+      int maxFaltas=5;
+      int media=60;
+      int res;
+      
+      if((nota >= media) && (faltas <= maxFaltas)){
+          System.out.println("Aprovado");
+      }else if (nota >=40){
+          System.out.println("Recuperação");
+      }else{
+          System.out.println("Reprovado");
+      }
+           
+      System.out.println("Fim do programa");
+    }
+    
+}
