167a9ec2a6ff2db488c2bbe5ec099a1ed52540fb
AgladeJesus <agladejesus@gmail.com>
2021-08-06 10:30:50 -0400

    Update C_CondicaoTernaria.java

--- CursoJava/src/cursojava/C_CondicaoTernaria.java
+++ CursoJava/src/cursojava/C_CondicaoTernaria.java
@@ -1,14 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package cursojava;
 
-/**
- *
- * @author aglad
- */
 public class C_CondicaoTernaria {
-    
+
+    public static void main(String[] args) {
+      int nota=85;
+      int faltas=10;
+      int maxFaltas=5;
+      int media=60;
+      int res;
+      
+      //CONDIÇÃO TERNÁRIA
+      /*res1=(nota >= media ? "Aprovado" : "Reprovado");
+      System.out.println("Resultado: " + res1);
+      */
+      
+      res=(nota >= media ? 1 : 0);
+      System.out.println("Resultado: " + (res==1?"Aprovado":"Reprovado"));
+      
+      System.out.println("Fim do programa");
+    }
 }
