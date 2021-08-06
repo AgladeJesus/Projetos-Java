ac5a4b786c0e8f05ddce04d70aa3f54edfc29239
AgladeJesus <agladejesus@gmail.com>
2021-08-06 16:13:22 -0400

    Update E_Scanner.java

--- CursoJava/src/cursojava/E_Scanner.java
+++ CursoJava/src/cursojava/E_Scanner.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class E_Scanner {
     public static void main(String[] args) {
-        
+        /*
         Scanner scan = new Scanner(System.in);
         int nota1=0;
         int nota2=0;
@@ -24,7 +24,24 @@ public class E_Scanner {
         media=(nota1+nota2+nota3)/3;
         System.out.println(media);
        // System.out.printf("A média da 1ª nota: d%, 2ª nota: d% e 3ª nota: d% é: d% ",nota1,nota2,nota3 media);
+       */
         
+        Scanner scan = new Scanner(System.in);
+        int nota1=0;
+        int nota2=0;
+        int nota3=0;
+        int media=0;
+        String aluno="Alexandre de Jesus Miranda";
+        
+        System.out.print("Qual o nome do aluno: ");
+        aluno=scan.nextLine();
+        System.out.print("Digite a 1ª nota: ");
+        nota1=scan.nextInt();
+        System.out.print("Digite a 2ª nota: ");
+        nota2=scan.nextInt();
+        System.out.print("Digite a 3ª nota: ");
+        nota3=scan.nextInt();
+        System.out.println("1ª Nota: " + nota1);
         
     }
     
