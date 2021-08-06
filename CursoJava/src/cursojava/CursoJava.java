334326d9f8ab33cf5e0974dc19ac3cc9b12eb695
Agladejesus <agladejesus@gmail.com>
2021-08-05 20:18:34 -0400

    Create CursoJava.java

---
+++ CursoJava/src/cursojava/CursoJava.java
@@ -0,0 +1,12 @@
+package cursojava;
+
+public class CursoJava {
+
+    public static void main(String[] args) {
+        System.out.print("CFBCursos\n");
+        System.out.println("CFBCursos");
+        System.out.printf("Canal:%s%nAno:%d\n", "CFB Cursos", 2021);
+        System.out.printf("Canal:%s\nAno:%d\n", "CFB Cursos", 2021);
+    }
+    
+}
