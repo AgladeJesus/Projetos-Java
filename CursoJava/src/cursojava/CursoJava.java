280774b79faebd057e45eb48b5214464ff68ed7d
AgladeJesus <agladejesus@gmail.com>
2021-08-06 09:35:26 -0400

    Update CursoJava.java

--- CursoJava/src/cursojava/CursoJava.java
+++ CursoJava/src/cursojava/CursoJava.java
@@ -3,25 +3,6 @@ package cursojava;
 public class CursoJava {
 
     public static void main(String[] args) {
-      /*
-        int num=0;
-        int num1=10;
-        int num2=20;
-        int num3=30;
-        int res = num + num1 + num2 + num3;
-        String nome="Alexandre de Jesus";
-        
-        num=100;
-        num=150;
-        num= num + 20;
-        
-        System.out.print("CFBCursos\n");
-        System.out.println("CFBCursos");
-        System.out.printf("Canal:%s%nAno:%d\n", "CFB Cursos", 2021);
-        System.out.printf("Canal:%s\nAno:%d\n", "CFB Cursos", 2021);
-        System.out.printf("Valor da variavel res:%d\nNome:%s%n%n",res, nome);
-        */
-      
       int nota=85;
       int faltas=10;
       int maxFaltas=5;
@@ -29,8 +10,8 @@ public class CursoJava {
       int res;
       
       //CONDIÇÃO TERNÁRIA
-      /*res=(nota >= media ? "Aprovado" : "Reprovado");
-      System.out.println("Resultado: " + res);
+      /*res1=(nota >= media ? "Aprovado" : "Reprovado");
+      System.out.println("Resultado: " + res1);
       */
       
       res=(nota >= media ? 1 : 0);
