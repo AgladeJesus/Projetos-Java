f5ca7afe845b5e8c8e34068dfb2b3461c6c6bfcb
Agladejesus <agladejesus@gmail.com>
2021-08-08 06:14:47 -0400

    Create H_Loop_DoWhile.java

---
+++ CursoJava/src/cursojava/H_Loop_DoWhile.java
@@ -0,0 +1,15 @@
+package cursojava;
+
+public class H_Loop_DoWhile {
+    public static void main(String[] args) {
+        
+        int cont=0;
+        
+        do{
+            System.out.println(cont + "- Jesus Cristo");
+            cont++;
+        }while(cont <= 0);
+        System.out.println("Fim do Programa");
+    }
+    
+}
