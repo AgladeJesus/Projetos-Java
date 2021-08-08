b77aeca693f28d315f4255d3bf590500975170e5
Agladejesus <agladejesus@gmail.com>
2021-08-08 06:02:37 -0400

    Create F_Loop_for.java

---
+++ CursoJava/src/cursojava/F_Loop_for.java
@@ -0,0 +1,17 @@
+package cursojava;
+
+public class F_Loop_for {
+    public static void main(String[] args) {
+        
+        for(int cont=0; cont < 5; cont++){
+            System.out.println(cont + " - CFB Cursos");
+        }
+        
+        for(int cont1=5; cont1 > 0; cont1--){
+            System.out.println(cont1 + " - CFB Cursos");
+        }
+        
+        System.out.println("Fim do programa");
+    }
+    
+}
