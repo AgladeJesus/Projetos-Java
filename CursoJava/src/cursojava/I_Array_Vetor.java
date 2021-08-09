d4db69fc79151f634ac4f7eb7655a3e17d4b101f
Agladejesus <agladejesus@gmail.com>
2021-08-08 21:22:23 -0400

    Update I_Array_Vetor.java

--- CursoJava/src/cursojava/I_Array_Vetor.java
+++ CursoJava/src/cursojava/I_Array_Vetor.java
@@ -1,14 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cursojava;
 
-/**
- *
- * @author aglad
- */
 public class I_Array_Vetor {
+    public static void main(String[] args){
+        
+        final int tam=5;
+     
+        int[] num=new int[tam];
+        int[] numeros={10,20,30,40,50};
+        num[0]=10;
+        num[1]=25;
+        num[2]=30;
+        num[3]=40;
+        num[4]=124;
+        
+        for(int i=0; i<numeros.length; i++){
+            System.out.printf("Número na posição: %d%n", numeros[i]);
+        }
+        for(int j=0; j<num.length; j++){
+            System.out.printf("Número na posição: %d%n", num[j]);
+        }
+        
+        System.out.printf("Fim do programa%n");
+        
+    }
     
 }
