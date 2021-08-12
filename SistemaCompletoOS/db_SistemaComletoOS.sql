39438ceecce1b9417fedaae4486956c650649869
Agladejesus <agladejesus@gmail.com>
2021-08-12 05:54:30 -0400

    Update db_SistemaComletoOS.sql

--- SistemaCompletoOS/db_SistemaComletoOS.sql
+++ SistemaCompletoOS/db_SistemaComletoOS.sql
@@ -39,3 +39,31 @@ values('Linus Torvalds','Rua Tux, 2015','9999-9999','linus@linus.com');
 
 select * from tbclientes;
 
+create table tbos(
+os int primary key auto_increment,
+data_os timestamp default current_timestamp,
+equipamento varchar(150) not null,
+defeito varchar(150) not null,
+servico varchar(150),
+tecnico varchar(30),
+valor decimal(10,2),
+idcli int not null,
+foreign key(idcli) references tbclientes(idcli)
+);
+
+select * from tbclientes;
+
+describe tbos;
+select * from tbos;
+
+insert into tbos(equipamento,defeito,servico,tecnico,valor,idcli)
+values('Notebook','Não Liga','Troca da fonte','Zé','87.50',1);
+
+select
+O.os,equipamento,defeito,servico, valor,
+C.nomecli,fonecli
+from tbos as O
+inner join tbclientes as C
+on (O.idcli = C.idcli);
+
+
