c1b5967a4e1cbff7eecd016660d80936fd14ee46
Agladejesus <agladejesus@gmail.com>
2021-08-14 09:19:14 -0400

    Update L_Metodos_Classe_Array1.java

--- CursoJava/src/cursojava/L_Metodos_Classe_Array1.java
+++ CursoJava/src/cursojava/L_Metodos_Classe_Array1.java
@@ -8,14 +8,18 @@ public class L_Metodos_Classe_Array1 {
         final int tam=10;
         int[] num={9,2,7,1,8,5,3,4,0,6};
         int[] numeros=new int[tam];
+        int p=3;
         
         //Arrays.sort(num);
         //Arrays.fill(numeros, 10);
-        System.arraycopy(num, 0, numeros, 0, tam);
+        //System.arraycopy(num, 0, numeros, 0, tam);
         
         
-        System.out.printf("Arrays são iquais: %s",Arrays.equals(num, numeros) ? "Sim":"Não");
+        //System.out.printf("Arrays são iquais: %s",Arrays.equals(num, numeros) ? "Sim":"Não");
         
+        Arrays.sort(num);
+        System.out.printf("O elemento %d está no array? %s",p,Arrays.binarySearch(num, p) > 0 ? "Sim":"Não");
+
         /*
         for(int n:num){
             System.out.printf("%d - ", n);
