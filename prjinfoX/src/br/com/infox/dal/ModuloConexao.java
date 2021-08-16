1a220df75c1160d4c3803f3d131a9f86c667240a
Agladejesus <agladejesus@gmail.com>
2021-08-16 05:25:59 -0400

    Update ModuloConexao.java

--- prjinfoX/src/br/com/infox/dal/ModuloConexao.java
+++ prjinfoX/src/br/com/infox/dal/ModuloConexao.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.infox.dal;
 
-/**
- *
- * @author aglad
- */
+import java.sql.*;
+
+
 public class ModuloConexao {
+    //Método responsavél por estabelecer a conexão com o banco
+    public static Connection conector(){
+        java.sql.Connection conexao = null;
+        //a linha abaixo "chama" o driver
+        String driver = "com.mysql.jbdc.Driver";
+        //Armazenando informaçãoes referente a banco
+        String url="jdbc:mysql://localhost:3306/dbinfox";
+        String user="root";
+        String password = "root";
+        // Estabelecendo a conexão com banco
+        try {
+            Class.forName(driver);
+            conexao = DriverManeger.getConnection(url, user, password);
+            return conexao;
+        } catch (Exception e) {
+            return null;
+        }
+    }
     
 }
