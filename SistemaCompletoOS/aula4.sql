2a1c8e4dbc11e30e265c80fb3c94829d18af856d
Agladejesus <agladejesus@gmail.com>
2021-08-18 05:31:46 -0400

    Create aula4.sql

---
+++ SistemaCompletoOS/aula4.sql
@@ -0,0 +1,12 @@
+use dbaula4;
+create table `curso`(
+	`Silga` CHAR(10),
+    `Nome` VARCHAR(64),
+    `Descricao` VARCHAR(256)
+);
+
+drop table cruso;
+describe curso;
+select * from curso;
+
+insert into(Sigla,Nome,Descricao)
